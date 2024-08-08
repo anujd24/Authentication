3fb1c1c5265f0b2fbd75c5826c421e4255beb215
Anuj <141406967+anujd24@users.noreply.github.com>
2024-08-08 23:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
+const jwtPassword = "12345";
 
 const app = express();
 app.use(express.json());
